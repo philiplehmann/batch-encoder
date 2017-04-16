10e0acb388c28601f932da4157d724200f821e3e
Philip Lehmann <Philip Lehmann>
2017-04-16 17:13:41 +0200

    binary update

--- src/config_win.js
+++ src/config_win.js
@@ -1,5 +1,9 @@
 'use strict'
 
+const path = require('path')
+
+const ROOT_DIR = path.dirname(__dirname)
+
 exports.binaries = {
   FFMPEG: path.join(ROOT_DIR, 'bin', 'ffmpeg.exe'),
   FFPROBE: path.join(ROOT_DIR, 'bin', 'ffprobe.exe'),
